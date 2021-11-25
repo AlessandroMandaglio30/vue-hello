70d9092f7f77773ab5f5340268051d25c1ef5022
Alessandro Mandaglio <ale.manda.30@gmail.com>
2021-11-25 15:49:44 +0100

    Eserczio completo con bonus

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const app = new Vue({
+    el: '#root',
+    data: {
+        message: 'Forza Inter',
+        img: 'https://data.nssmag.com/images/galleries/26544/cover-mockup-inter.png'
+    }
+
+});
